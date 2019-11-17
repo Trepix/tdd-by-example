5dd1d87a649a0cb2f2b3e3f6a9ac19dcb26a413d
Trepix <pau.trepat.segura@gmail.com>
2019-11-17 15:08:47 +0100

    Chapter 19 # Set The Table

--- xunit-example/WasRun.py
+++ xunit-example/WasRun.py
@@ -4,7 +4,11 @@ from TestCase import TestCase
 class WasRun(TestCase):
     def __init__(self, name):
         self.wasRun = None
+        self.wasSetUp = None
         TestCase.__init__(self, name)
 
     def testMethod(self):
         self.wasRun = 1
+
+    def setUp(self):
+        self.wasSetUp = 1
--- xunit-example/todo-list.md
+++ xunit-example/todo-list.md
@@ -2,8 +2,8 @@
 
 ## ToDo List
 
-* **~~Invoke test method~~**
-* Invoke setUp first
+* ~~Invoke test method~~
+* **~~Invoke setUp first~~**
 * Invoke tearDown afterward
 * Invoke tearDown even if the test method fails
 * Run multiple tests
