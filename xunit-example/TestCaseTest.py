c6e0c5c4c65c3039fea333ddfe6ffe11dc7db0be
Trepix <pau.trepat.segura@gmail.com>
2019-11-17 14:49:26 +0100

    Chapter 18 # First Steps to xUnit

---
+++ xunit-example/TestCaseTest.py
@@ -0,0 +1,14 @@
+from WasRun import WasRun
+from TestCase import TestCase
+
+
+class TestCaseTest(TestCase):
+
+    def testRunning(self):
+        test = WasRun("testMethod")
+        assert (not test.wasRun)
+        test.run()
+        assert test.wasRun
+
+
+TestCaseTest("testRunning").run()
---
+++ xunit-example/todo-list.md
@@ -0,0 +1,10 @@
+# xUnit example
+
+## ToDo List
+
+* **~~Invoke test method~~**
+* Invoke setUp first
+* Invoke tearDown afterward
+* Invoke tearDown even if the test method fails
+* Run multiple tests
+* Report collected results
