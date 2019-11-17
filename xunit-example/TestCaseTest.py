e75de76ab7e2d9cd5577a6248563485668354aac
Trepix <pau.trepat.segura@gmail.com>
2019-11-18 00:36:03 +0100

    Chapter 23 # How Suite It Is

--- xunit-example/TestCaseTest.py
+++ xunit-example/TestCaseTest.py
@@ -1,36 +1,48 @@
 from TestResult import TestResult
 from WasRun import WasRun
 from TestCase import TestCase
+from TestSuite import TestSuite
 
 
 class TestCaseTest(TestCase):
 
     def setUp(self):
-        pass
+        self.result = TestResult()
 
     def testTemplateMethod(self):
         test = WasRun("testMethod")
-        test.run()
+        test.run(self.result)
         assert ("setUp testMethod tearDown " == test.log)
 
     def testResult(self):
         test = WasRun("testMethod")
-        result = test.run()
-        assert("1 run, 0 failed" == result.summary())
+        test.run(self.result)
+        assert("1 run, 0 failed" == self.result.summary())
 
     def testFailedResult(self):
         test = WasRun("testBrokenMethod")
-        result = test.run()
-        assert ("1 run, 1 failed" == result.summary())
+        test.run(self.result)
+        assert ("1 run, 1 failed" == self.result.summary())
 
     def testFailedResultFormatting(self):
-        result = TestResult()
-        result.testStarted()
-        result.testFailed()
-        assert ("1 run, 1 failed" == result.summary())
+        self.result.testStarted()
+        self.result.testFailed()
+        assert ("1 run, 1 failed" == self.result.summary())
+
+    def testSuite(self):
+        suite = TestSuite()
+        suite.add(WasRun("testMethod"))
+        suite.add(WasRun("testBrokenMethod"))
+        suite.run(self.result)
+        assert("2 run, 1 failed" == self.result.summary())
 
 
-TestCaseTest("testTemplateMethod").run()
-TestCaseTest("testResult").run()
-TestCaseTest("testFailedResult").run()
-TestCaseTest("testFailedResultFormatting").run()
+suite = TestSuite()
+suite.add(TestCaseTest("testTemplateMethod"))
+suite.add(TestCaseTest("testResult"))
+suite.add(TestCaseTest("testFailedResult"))
+suite.add(TestCaseTest("testFailedResultFormatting"))
+suite.add(TestCaseTest("testSuite"))
+suiteResult = TestResult()
+suite.run(suiteResult)
+print(suiteResult.summary())
--- xunit-example/todo-list.md
+++ xunit-example/todo-list.md
@@ -6,6 +6,9 @@
 * ~~Invoke setUp first~~
 * ~~Invoke tearDown afterward~~
 * Invoke tearDown even if the test method fails
-* Run multiple tests
-* **Report collected results**
+* ~~**Run multiple tests**~~
+* ~~Report collected results~~
 * ~~Log string in WasRun~~
+* ~~Report failed tests~~
+* Catch and report setUp errors
+* Create TestSuite from a TestCase class
